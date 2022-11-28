44822fec4722d3727f8be76e10f460605b962e65
bk <binit.katiyar@toffeeinsurance.com>
2022-11-28 15:14:38 +0530

    Added error boundy component using class based components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
+// import {ErrorBoundary} from 'react-error-boundary'
+// import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import { BrowserRouter, Route, Switch } from "react-router-dom";
 // import TestHooks from './components/TestHooks';
 // import useCustomHook from './hooks/useCustomHook';
 // import SearchText from './components/SearchText'; 
+import ErrorBoundary from './components/ErrorBoundary';
+import BuggyCounter  from './components/BuggyCounter';
 
-const RouteWithLayout = ({ component: Component, layout: Layout, ...rest }) => (
+/* const RouteWithLayout = ({ component: Component, layout: Layout, ...rest }) => (
   <Route {...rest} render={props => (
     <Layout>
       <Component {...props} />
@@ -33,11 +37,25 @@ const Foo = () => (
 
 const Bar = () => (
   <p>Bar Component</p>
-)
+) */
 
+/* function ErrorFallback({error, resetErrorBoundary}) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  )
+}
+
+function Bomb() {
+  throw new Error('💥 CABOOM 💥')
+} */
 
 function App() {
   // useCustomHook();
+  // const [explode, setExplode] = useState(false)
 
   return (
     <div className="App">
@@ -48,17 +66,51 @@ function App() {
         </p> */}
         
         {/* <ParentComponent /> */}
+
         {/* <TestHooks /> */}
+        
         {/* <SearchText /> */}
-        <BrowserRouter>
+        
+        {/* <BrowserRouter>
           <Switch>
             <RouteWithLayout exact path="/foo" layout={MainLayout} component={Foo} />
-            <RouteWithLayout exact path="/fizz" layout={MainLayout} component={Foo} />
-
+            <RouteWithLayout exact path="/" layout={MainLayout} component={Foo} />
             <RouteWithLayout exact path="/bar" layout={AltLayout} component={Bar} />
-            <RouteWithLayout exact path="/buzz" layout={AltLayout} component={Foo} />
           </Switch>
-        </BrowserRouter>
+        </BrowserRouter> */}
+
+        <div>
+          <p>
+            <b>
+              This is an example of error boundaries in React 16.
+              <br /><br />
+              Click on the numbers to increase the counters.
+              <br />
+              The counter is programmed to throw when it reaches 5. This simulates a JavaScript error in a component.
+            </b>
+          </p>
+          <hr />
+          <ErrorBoundary>
+            <p>These two counters are inside the same error boundary. If one crashes, the error boundary will replace both of them.</p>
+            <BuggyCounter />
+            <BuggyCounter />
+          </ErrorBoundary>
+          <hr />
+          <p>These two counters are each inside of their own error boundary. So if one crashes, the other is not affected.</p>
+          <ErrorBoundary><BuggyCounter /></ErrorBoundary>
+          <ErrorBoundary><BuggyCounter /></ErrorBoundary>
+        </div>
+
+        {/* <div>
+          <button onClick={() => setExplode(e => !e)}>toggle explode</button>
+          <ErrorBoundary
+            FallbackComponent={ErrorFallback}
+            onReset={() => setExplode(false)}
+            resetKeys={[explode]}
+          >
+            {explode ? <Bomb /> : null}
+          </ErrorBoundary>
+        </div> */}
       </header>
     </div>
   );
